485d9004ac502dc64d5e88033405de98ff0037a9
Cerqueira <joaokruschewsky@gmail.com>
2025-08-12 15:21:03 -0300

    upload some tests pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6", // azul
+        secondary: "#10B981", // verde
+      },
+    },
+  },
+  plugins: [],
+};
